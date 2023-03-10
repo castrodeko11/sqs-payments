acbdab63e1a969039c31b97f24b156ef6ad9253e
castrodeko11 <castrodeko11@hotmail.com>
2023-03-09 22:07:10 -0300

    Client SQS and LocalStack

--- src/main/kotlin/com/aco/taxpayments/config/SQSClientConfiguration.kt
+++ src/main/kotlin/com/aco/taxpayments/config/SQSClientConfiguration.kt
@@ -1,7 +1,9 @@
 package com.aco.taxpayments.config
 
+import com.amazonaws.SDKGlobalConfiguration
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.client.builder.AwsClientBuilder
 import com.amazonaws.regions.Regions
 import com.amazonaws.services.sqs.AmazonSQS;
 import com.amazonaws.services.sqs.AmazonSQSClientBuilder;
@@ -13,6 +15,9 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 class SQSClientConfiguration {
 
+    @Value("\${aws.sqs.endpoint}")
+    private val endpoint: String? = null
+
     @Value("\${aws.access_key_id}")
     private val awsAccessKeyId: String? = null
 
@@ -20,14 +25,16 @@ class SQSClientConfiguration {
     private val awsSecretKeyId: String? = null
 
     @Value("\${aws.region}")
-    private val awsRegion: String? = null
+    private val region: String? = null
 
     @Bean
     fun amazonSQSClient(): AmazonSQS? {
-        val awsCredentials =
-            BasicAWSCredentials(awsAccessKeyId, awsSecretKeyId)
-        return AmazonSQSClientBuilder.standard()
-            .withCredentials(AWSStaticCredentialsProvider(awsCredentials))
-            .withRegion(Regions.US_EAST_1).build()
+        System.setProperty(SDKGlobalConfiguration.DISABLE_CERT_CHECKING_SYSTEM_PROPERTY, "true")
+
+        return AmazonSQSClientBuilder
+            .standard()
+            .withEndpointConfiguration(AwsClientBuilder.EndpointConfiguration(endpoint, region))
+            .withCredentials(AWSStaticCredentialsProvider(BasicAWSCredentials(awsAccessKeyId, awsSecretKeyId)))
+            .build()
     }
 }
--- src/main/kotlin/com/aco/taxpayments/service/MessageQueueService.kt
+++ src/main/kotlin/com/aco/taxpayments/service/MessageQueueService.kt
@@ -3,6 +3,7 @@ package com.aco.taxpayments.service
 import com.amazonaws.services.sqs.AmazonSQS
 import com.amazonaws.services.sqs.model.*
 import lombok.RequiredArgsConstructor
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Service
@@ -12,9 +13,9 @@ import org.springframework.stereotype.Service
 @RequiredArgsConstructor
 internal class MessageQueueService {
     @Value("\${app.config.message.queue.topic}")
-
     private val messageQueueTopic: String? = null
 
+    @Autowired
     private val amazonSQSClient: AmazonSQS? = null
 
     @Scheduled(fixedDelay = 5000) // executes on every 5 second gap.

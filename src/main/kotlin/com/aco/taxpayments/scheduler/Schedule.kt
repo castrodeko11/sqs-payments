ebfd432a6379f152bc4199f1fab06afe749d9f1e
André Castro de Oliveira <castrodeko11@hotmail.com>
2023-03-26 22:48:10 -0300

    SQS Asyncs and Listener

---
+++ src/main/kotlin/com/aco/taxpayments/scheduler/Schedule.kt
@@ -0,0 +1,19 @@
+package com.aco.taxpayments.scheduler
+
+import org.slf4j.LoggerFactory
+import org.springframework.scheduling.annotation.EnableScheduling
+import org.springframework.scheduling.annotation.Scheduled
+import org.springframework.stereotype.Component
+import java.time.LocalDateTime
+
+@Component
+@EnableScheduling
+class Schedule {
+
+    private val logger = LoggerFactory.getLogger(javaClass)
+
+    @Scheduled(fixedDelay = 5000)
+    fun receiveMessageSync() {
+        logger.info("\nScheduled: ${LocalDateTime.now()}")
+    }
+}
---
+++ src/main/kotlin/com/aco/taxpayments/web/controller/QueueController.kt
@@ -0,0 +1,29 @@
+package com.aco.taxpayments.web.controller
+
+import com.amazonaws.services.sqs.AmazonSQSAsync
+import com.amazonaws.services.sqs.model.SendMessageRequest
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
+
+@RestController
+@RequestMapping("/queue")
+class QueueController(
+
+    @Value("\${cloud.aws.queue.endpoint}") private val queueEndpoint: String,
+    @Value("\${cloud.aws.queue.tax-payments}") private val queueName: String,
+    var sqsClientAsycs: AmazonSQSAsync
+) {
+
+    @PostMapping
+    fun sendMessage(@RequestBody message: String): ResponseEntity<String> {
+
+        val sendMessageStandardQueue = SendMessageRequest()
+            .withMessageBody(message)
+            .withQueueUrl("$queueEndpoint$queueName")
+
+        val result = sqsClientAsycs.sendMessage(sendMessageStandardQueue)
+
+        return ResponseEntity.ok().body(result.messageId)
+    }
+}
